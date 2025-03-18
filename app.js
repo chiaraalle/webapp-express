e94a466ae03ef1162c67a00ab606a90b44620ed6
chiaralle <chiaraallegrino@gmail.com>
2025-03-18 15:54:36 +0100

    add: i cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import movieRouter from './routes/movieRouter.js';
-
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
@@ -9,6 +9,12 @@ const port = 3000;
 app.get( '/', (req, res) => {
   res.send( 'Server movie tutto a posto!' )
 } )
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_APP,
+  })
+);
 
 //middleware per gestire asset statici
 app.use( express.static('public') )
